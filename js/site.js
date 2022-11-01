37203114cca553a786d4e63e75b6bd429b968b0e
Regber <regber.888@gmail.com>
2022-11-01 18:13:29 +0300

    First commit

---
+++ js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).on("click", ".panel-side>div >ul>li", function () {
+    let currentElement = $(this);
+    let allElements = $(".panel-side >div>ul>li");
+
+    allElements.each(function () {
+        if ($(this).is(currentElement)) {
+            if (currentElement.hasClass("selected")) {
+                currentElement.removeClass("selected");
+            }
+            else {
+                currentElement.addClass("selected");
+            }
+        }
+        else {
+            $(this).removeClass("selected");
+        }
+    });
+});
+$(document).on("click", ".panel-side >div>ul>li>ul", function (e) {
+    e.stopPropagation()
+});
